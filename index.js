ae1898ea6fa7346e5c3787ddb733fc9146d64e80
vinusingh36 <vineet.vinusingh36@gmail.com>
2024-05-23 00:42:46 +0530

    db json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { create, router as _router, defaults } from "json-server"; // importing json-server library
-const server = create();
-const router = _router("db.json");
-const middlewares = defaults();
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
